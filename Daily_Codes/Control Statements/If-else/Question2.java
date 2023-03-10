ad31d7cc89fa75aedbe803039d678064268e4614
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-11 00:38:56 +0530

    Update Question2.java

--- Daily_Codes/Control Statements/If-else/Question2.java
+++ Daily_Codes/Control Statements/If-else/Question2.java
@@ -1,3 +1,10 @@
+// Take two integers A and B as input 
+// Print the max of two
+// Assume : x and y are not equal
+
+// Input :  x = 5 , y = 7
+// Output : 7 is greater
+
 class Question2 {
 	public static void main(String[]args){
 
@@ -5,10 +12,10 @@ class Question2 {
 		int y = 7;
 
 		if(x>y){
-		System.out.println(x+ "is greater");
+		System.out.println(x+ " is greater");
 		}
 		else{
-		System.out.println(y+ "is greater");
+		System.out.println(y+ " is greater");
 		}
 	}
 }
